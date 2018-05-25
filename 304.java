a72241e3c4eed639c2c97c335fe3a72536d10881
k840304 <k840304@yahoo.com.tw>
2018-05-25 16:21:33 +0800

    Update 304.java

--- 304.java
+++ 304.java
@@ -7,30 +7,24 @@ import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.widget.Toast; 
 
-
-
 public class GDD03_SM_Receiver extends BroadcastReceiver 
 { 
-   
- 
-  
   @Override 
   public void onReceive(Context context, Intent intent) 
   { 
     // TODO Auto-generated method stub 
     Bundle bundle = intent.getExtras();
     Object[] pdus = (Object[]) bundle.get("pdus");
-	SmsMessage sms;
-	String str = "";
-	for(Object pdu : pdus){
-		sms = SmsMessage.createFromPdu((byte[]) pdu);
-		str += "接收到來自:\n";
-		str += sms.getOriginatingAddress().toString() + "\n";
-		str += "------傳來的簡訊------\n";
-		str += sms.getDisplayMessageBody();
-	}
-	Toast.makeText(context, str, Toast.LENGTH_LONG).show();
-	  
+    SmsMessage sms;
+    String str = "";
+    for(Object pdu : pdus){
+        sms = SmsMessage.createFromPdu((byte[]) pdu);
+        str += "接收到來自:\n";
+        str += sms.getOriginatingAddress().toString() + "\n";
+        str += "------傳來的簡訊------\n";
+        str += sms.getDisplayMessageBody();
+    }
+    Toast.makeText(context, str, Toast.LENGTH_LONG).show();  
   } 
 } 
 
